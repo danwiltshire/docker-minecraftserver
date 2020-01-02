6dbd9e1e14d2f95d31e529191f760554f7b8c9b4
Daniel Wiltshire <me@danielwiltshire.co.uk>
2020-01-02 19:13:12 +0000

    basic tests

---
+++ tests/google-cst/get-cst.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ex
+
+echo "Getting Google Container Structure Test..."
+
+curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-amd64 && chmod +x container-structure-test-linux-amd64 && mkdir -p $HOME/bin && export PATH=$PATH:$HOME/bin && mv container-structure-test-linux-amd64 $HOME/bin/container-structure-test
---
+++ tests/google-cst/run-cst.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+container-structure-test test -v DEBUG --image docker-minecraftserver_paper --config tests/google-cst/cst-config.yml
