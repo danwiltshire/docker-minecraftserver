ec44a8d4db50872058ce528a9960d0d5aa584c2c
Daniel Wiltshire <me@danielwiltshire.co.uk>
2020-01-03 11:02:09 +0000

    added minestat

---
+++ tests/minestat/status-loop.py
@@ -0,0 +1,22 @@
+import sys
+import time
+import minestat
+
+server = '127.0.0.1'
+port = 25565
+timeout = time.time() + 120 # How long in seconds to keep retrying
+retryTimeout = 5 # How long in seconds between retries
+
+print('Checking status of ' + server + ':' + str(port) + '...')
+
+while True:
+  ms = minestat.MineStat(server, port)
+  if ms.online:
+    print('The server is online.')
+    break
+  elif time.time() > timeout:
+    print('No response and timeout has lapsed, exiting.')
+    sys.exit(1)
+  else:
+    time.sleep(retryTimeout)
+    print('No response, retrying in ' + str(retryTimeout) + ' seconds...')
